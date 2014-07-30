f890f298bb03c7d42668fd8fdabc6ca784bfa54b
Chris Marisic <chris@marisic.com>
2014-07-30 09:21:04 -0400

    Create Expand shorthand class and tests

---
+++ Dynamitey/Expando.cs
@@ -0,0 +1,32 @@
+﻿using System.Dynamic;
+using Dynamitey.DynamicObjects;
+
+namespace Dynamitey
+{
+    public class Expando : Builder<ExpandoObject>
+    {
+        // ReSharper disable StaticFieldInGenericType
+        private static readonly dynamic _expandoBuilder = new Builder<ExpandoObject>().Object;
+        // ReSharper restore StaticFieldInGenericType        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Expando"/> class.
+        /// This constructor is shorthand for new Builder&lt;ExpandoObject>();
+        /// </summary>
+        public Expando()
+        {
+        }
+
+        /// <summary>
+        /// Gets the new expandoObject builder. This method is short hand for Build&gt;ExpandoObject>.NewObject()
+        /// </summary>
+        /// <value>The new expandoObject.</value>
+        public static dynamic New
+        {
+            get
+            {
+                return _expandoBuilder;
+            }
+        }        
+    }
+}
